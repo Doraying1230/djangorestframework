11dea0533c97f22788d1dfce14f4117f0db9dc4e
a289237642 <289237642@qq.com>
2018-08-19 15:51:55 +0800

    819

--- testdayday/testdayday/settings.py
+++ testdayday/testdayday/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'testdayday',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'mysql',
         'HOST': 'localhost',
         'PORT': '3306',
     }
